5ce6d7982a9e8daae69a8a849effce675f7dc7c8
Maksim Maksimov <maximebayer@gmail.com>
2021-12-25 23:03:07 -0500

    modify test_quest_can_go_to_login_page with initialize instance

--- pages/test_main_page.py
+++ pages/test_main_page.py
@@ -1,11 +1,14 @@
 from .main_page import MainPage
+from .login_page import LoginPage
 
 
 def test_quest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link) # initial Page Object, send to constructor instance of driver and url
+    # page is instance of MainPage class with two arguments
     page.open()                    # open page by url
-    page.go_to_login_page()        # execute method "go_to_login_page" go to login page
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_quest_should_see_login_link(browser):
