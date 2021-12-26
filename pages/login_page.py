ae6f49403024cbdb0b51e81df73bae001b5b6fd6
Maksim Maksimov <maximebayer@gmail.com>
2021-12-25 23:06:03 -0500

    add assert for checking login link

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_LINK), "Login link is not presented"
+        assert self.browser.current_url, "Login url is not presented"
         # assert True (mock stub)
 
     def should_be_login_form(self):
