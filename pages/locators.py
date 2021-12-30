8c5551da24976e030172701ff02a2a9e53c088e7
Maksim Maksimov <maximebayer@gmail.com>
2021-12-29 23:57:10 -0500

    add locators for product pade and basket page

--- pages/locators.py
+++ pages/locators.py
@@ -1,13 +1,12 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
-
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
-class LoginPageLocators():
+class LoginPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    
+
     LOGIN_FORM = (By.ID, "login_form")
     LOGIN_EMAIL = (By.NAME, 'login-username')
     LOGIN_PASSWORD = (By.NAME, "login-password")
@@ -18,6 +17,14 @@ class LoginPageLocators():
     REGISTER_PASSWORD = (By.NAME, "registration-password1")
     CONFIRM_REGISTER_PASSWORD = (By.NAME, "registration-password2")
     REGISTER_BUTTON = (By.NAME, "registration_submit")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, "button[value='Add to basket']")
+    ITEM_PRICE = (By.CSS_SELECTOR, "h1+p.price_color")
+    ITEM_NAME = (By.CSS_SELECTOR, "h1")
+    BASKET_MESSAGE = (By.CSS_SELECTOR, "div:nth-child(1) > div[class = 'alertinner '] strong")
+    BASKET_ITEM_PRICE = (By.CSS_SELECTOR, "div[class = 'alertinner '] p strong")
 
 
 
