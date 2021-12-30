29058c394f92f6f887af4350e596033ad8705e17
Maksim Maksimov <maximebayer@gmail.com>
2021-12-29 23:55:33 -0500

    add product page properties: url, price, add_basket_btn, compare prices, name item, check successful messages

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,16 +1,41 @@
-from selenium.common.exceptions import NoAlertPresentException # в начале файла
+from selenium.common.exceptions import NoAlertPresentException  # в начале файла
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
 
 
-def solve_quiz_and_get_code(self):
-    alert = self.browser.switch_to.alert
-    x = alert.text.split(" ")[2]
-    answer = str(math.log(abs((12 * math.sin(float(x))))))
-    alert.send_keys(answer)
-    alert.accept()
-    try:
-        alert = self.browser.switch_to.alert
-        alert_text = alert.text
-        print(f"Your code: {alert_text}")
-        alert.accept()
-    except NoAlertPresentException:
-        print("No second alert presented")
+class ProductPage(BasePage):
+
+    def should_be_product_page(self):
+        self.should_be_product_url()
+        self.should_be_item_price()
+        self.should_be_add_to_basket_button()
+        self.should_be_name_of_item()
+
+    def should_be_product_url(self):
+        assert self.browser.current_url, "Product page url is not presented"
+
+    def should_be_item_price(self):
+        assert self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text, "Price is not presented"
+
+    def should_be_add_to_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BTN), "'Add to basket' button is not " \
+                                                                                "presented "
+
+    def should_be_name_of_item(self):
+        item_price = self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
+        assert item_price.is_element_present, "Name of item is not presented"
+
+    def should_be_available_click_add_to_basket(self):
+        basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
+        basket.click()
+
+    def should_be_message_added_to_basket(self):
+        added_to_basket_message = self.browser.find_element(*ProductPageLocators.BASKET_MESSAGE).text
+        assert added_to_basket_message == "The shellcoder's handbook", "Message of successfully adding item to basket " \
+                                                                       "isn't presented "
+
+    def basket_price_equals_product_page_price(self):
+        basket_price = self.browser.find_element(*ProductPageLocators.BASKET_ITEM_PRICE).text
+        item_price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
+        assert item_price == basket_price, "Price on product page and basket page isn't equal"
