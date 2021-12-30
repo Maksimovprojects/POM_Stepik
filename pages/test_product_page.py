a0b313084687197eceb37405da0c3cc3886cb8b0
Maksim Maksimov <maximebayer@gmail.com>
2021-12-30 00:50:31 -0500

    add parametrize decorator for testing 10 urls

--- pages/test_product_page.py
+++ pages/test_product_page.py
@@ -1,8 +1,5 @@
 import time
-from selenium.common.exceptions import NoAlertPresentException  # в начале файла
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-from .locators import ProductPageLocators
+import pytest
 from .product_page import ProductPage
 
 
@@ -15,15 +12,33 @@ def test_quest_can_go_to_product_page(browser):
     product_page.should_be_product_page()
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     product_page = ProductPage(browser, browser.current_url)
     product_page.should_be_available_click_add_to_basket()
     product_page.test_solve_quiz_and_get_code()
-    product_page.should_be_message_added_to_basket()
+    product_page.should_be_name_of_item_added_to_basket()
     product_page.basket_price_equals_product_page_price()
 
 
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket_list_links(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    product_page = ProductPage(browser, browser.current_url)
+    product_page.should_be_available_click_add_to_basket()
+    product_page.test_solve_quiz_and_get_code()
+    product_page.should_be_name_of_item_added_to_basket()
+    product_page.basket_price_equals_product_page_price()
 
 
