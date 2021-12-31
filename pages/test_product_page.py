e985e62dcb87c98a271a84bb426cb4306f18cbe4
Maksim Maksimov <maximebayer@gmail.com>
2021-12-31 14:55:32 -0500

    add 'test_guest_can_go_to_login_page_from_product_page'

--- pages/test_product_page.py
+++ pages/test_product_page.py
@@ -1,6 +1,7 @@
 import time
 import pytest
 from .product_page import ProductPage
+from selenium.webdriver.common import alert
 
 
 def test_quest_can_go_to_product_page(browser):
@@ -10,6 +11,7 @@ def test_quest_can_go_to_product_page(browser):
     page.open()  # open page by url
     product_page = ProductPage(browser, browser.current_url)
     product_page.should_be_product_page()
+
 
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -43,14 +45,15 @@ def test_guest_can_add_product_to_basket_list_links(browser, link):
 
 
 # def test_train_abstract_methods(browser, link):
-
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): # mark as fail test
+@pytest.mark.xfail(reason="negative abstract testcase")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):  # mark as fail test
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     product_page = ProductPage(browser, browser.current_url)
     product_page.should_be_available_click_add_to_basket()
     product_page.should_not_be_success_message()
+
 
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -59,6 +62,10 @@ def test_guest_cant_see_success_message(browser):
     product_page = ProductPage(browser, browser.current_url)
     product_page.should_not_be_success_message()
 
+
+# 'pytest -v -k message pages/test_product_page.py' (command of selection '-k' methods having word "message" in name of
+# method )
+@pytest.mark.xfail(reason="negative abstract testcase")
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -68,9 +75,15 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.success_message_disappeared()
 
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
-
-
-
-
-
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    time.sleep(4)
