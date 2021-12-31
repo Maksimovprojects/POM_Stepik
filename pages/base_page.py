1331e77f3c12bd9a2c3068ddc9dbeeedb4bc5939
Maksim Maksimov <maximebayer@gmail.com>
2021-12-31 14:53:46 -0500

    moving methods from MainPage to BasePage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
 import math
-from .locators import BasePageLocators
+from selenium.webdriver.common import alert
+from .locators import BasePageLocators, LoginPageLocators
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
@@ -36,15 +37,15 @@ class BasePage:
             return False
         return True
 
-    class MainPage(BasePage):
-        def go_to_login_page(self):
-            login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
-            login_link.click()
-            alert = self.browser.switch_to.alert
-            alert.accept()
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*LoginPageLocators.LOGIN_LINK)
+        login_link.click()
 
-        def should_be_login_link(self):
-            assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+        # alert = self.browser.switch_to.alert
+        # alert.accept()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def test_solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
