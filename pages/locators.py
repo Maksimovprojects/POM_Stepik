d5b26019f396a008689d5488374a4000c317c218
Maksim Maksimov <maximebayer@gmail.com>
2021-12-31 14:52:57 -0500

    comment unused locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,22 +1,22 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators:
+
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
 class LoginPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-    LOGIN_FORM = (By.ID, "login_form")
-    LOGIN_EMAIL = (By.NAME, 'login-username')
-    LOGIN_PASSWORD = (By.NAME, "login-password")
-    LOGIN_BUTTON = (By.NAME, "login_submit")
-
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    # LOGIN_EMAIL = (By.NAME, 'login-username')
+    # LOGIN_PASSWORD = (By.NAME, "login-password")
+    # LOGIN_BUTTON = (By.NAME, "login_submit")
+    #
     REGISTER_FORM = (By.ID, "register_form")
-    REGISTER_EMAIL = (By.NAME, 'registration-email')
-    REGISTER_PASSWORD = (By.NAME, "registration-password1")
-    CONFIRM_REGISTER_PASSWORD = (By.NAME, "registration-password2")
-    REGISTER_BUTTON = (By.NAME, "registration_submit")
+    # REGISTER_EMAIL = (By.NAME, 'registration-email')
+    # REGISTER_PASSWORD = (By.NAME, "registration-password1")
+    # CONFIRM_REGISTER_PASSWORD = (By.NAME, "registration-password2")
+    # REGISTER_BUTTON = (By.NAME, "registration_submit")
 
 
 class ProductPageLocators:
@@ -25,7 +25,4 @@ class ProductPageLocators:
     ITEM_NAME = (By.CSS_SELECTOR, "h1")
     BASKET_MESSAGE = (By.CSS_SELECTOR, "div:nth-child(1) > div[class = 'alertinner '] strong")
     BASKET_ITEM_PRICE = (By.CSS_SELECTOR, "div[class = 'alertinner '] p strong")
-
-
-
-
+    SUCCESS_MESSAGE = (By.XPATH, "//*[text()[contains(.,'has been added to your basket')]]")
