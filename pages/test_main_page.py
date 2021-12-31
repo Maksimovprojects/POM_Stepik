de8540110488d649f2a52d30451dc92e3a36768e
Maksim Maksimov <maximebayer@gmail.com>
2021-12-31 18:59:08 -0500

    add tes 'test_guest_cant_see_product_in_basket_opened_from_main_page'

--- pages/test_main_page.py
+++ pages/test_main_page.py
@@ -1,14 +1,17 @@
+import time
+
 from .main_page import MainPage
 from .login_page import LoginPage
+from .basket_page import BasketPage
 
 
 def test_quest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link) # initial Page Object, send to constructor instance of driver and url
+    main_page = MainPage(browser, link)  # initial Page Object, send to constructor instance of driver and url
     # page is instance of MainPage class with two arguments
-    page.open()                    # open page by url
+    main_page.open()  # open page by url
+    main_page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
-    login_page.go_to_login_page()
     login_page.should_be_login_page()
 
 
@@ -17,3 +20,11 @@ def test_quest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.should_go_to_the_basket()
+    page.should_be_empty_basket()
