36b3f43e3bb80e57dd3e8e646acc56f722ba7f05
Maksim Maksimov <maximebayer@gmail.com>
2021-12-31 19:11:17 -0500

    add locators to class BasketPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -3,20 +3,18 @@ from selenium.webdriver.common.by import By
 
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    BASKET_BUTTON = (By.CSS_SELECTOR, "//a[@class='btn btn-default']")
 
 
 class LoginPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    # LOGIN_EMAIL = (By.NAME, 'login-username')
-    # LOGIN_PASSWORD = (By.NAME, "login-password")
-    # LOGIN_BUTTON = (By.NAME, "login_submit")
-    #
     REGISTER_FORM = (By.ID, "register_form")
-    # REGISTER_EMAIL = (By.NAME, 'registration-email')
-    # REGISTER_PASSWORD = (By.NAME, "registration-password1")
-    # CONFIRM_REGISTER_PASSWORD = (By.NAME, "registration-password2")
-    # REGISTER_BUTTON = (By.NAME, "registration_submit")
+
+
+class BasketPageLocators:
+    BASKET_BUTTON = (By.XPATH, "//a[@class='btn btn-default']")
+    EMPTY_BASKET_MSG = (By.XPATH, "//*[contains(text(), 'Your basket is empty')]")
 
 
 class ProductPageLocators:
