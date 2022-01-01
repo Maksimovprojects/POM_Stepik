79414b693cd4cd1773d6da4f6cc5e4e4646963e2
Maksim Maksimov <maximebayer@gmail.com>
2021-12-31 21:26:46 -0500

    bound two tests to TestLoginFromMainPage class and mark class fixture marker 'login_quest'

--- pages/test_main_page.py
+++ pages/test_main_page.py
@@ -1,25 +1,26 @@
-import time
-
+import pytest
 from .main_page import MainPage
 from .login_page import LoginPage
 from .basket_page import BasketPage
 
 
-def test_quest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    main_page = MainPage(browser, link)  # initial Page Object, send to constructor instance of driver and url
-    # page is instance of MainPage class with two arguments
-    main_page.open()  # open page by url
-    main_page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_quest
+class TestLoginFromMainPage:
 
+    def test_quest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        main_page = MainPage(browser, link)  # initial Page Object, send to constructor instance of driver and url
+        # page is instance of MainPage class with two arguments
+        main_page.open()  # open page by url
+        main_page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)  # initialization as instance of class LoginPage
+        login_page.should_be_login_page()
 
-def test_quest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_quest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
