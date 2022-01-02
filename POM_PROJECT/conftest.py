b73118c2610e85299b45ad903ee5dfe9e8e8117d
Maksim Maksimov <maximebayer@gmail.com>
2022-01-01 20:09:49 -0500

    delete cach files

--- POM_PROJECT/conftest.py
+++ POM_PROJECT/conftest.py
@@ -7,6 +7,7 @@ def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en',
                      help="Choose language= ru, n, fi, de, en-gb, fr, etc")
 
+
 @pytest.fixture
 def browser(request):
     user_language = request.config.getoption("language")
